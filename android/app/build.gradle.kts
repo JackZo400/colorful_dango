5e54d6e25d8c0c5d36793c65f3760b504af54662
Jack White <JackWhite114514@outlook.com>
2026-07-02 00:02:29 +0800

    🔏 APK签名

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,9 +27,12 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.create("release") {
+                storeFile = file("upload-keystore.jks")
+                storePassword = "colorfuldango"
+                keyAlias = "upload"
+                keyPassword = "colorfuldango"
+            }
         }
     }
 }
